77a9a2d01f4dd33fcc95d4a691db5bf4c9711a26
Романов Михайло <romanovmisha99@gmail.com>
2025-09-16 15:41:22 +0300

    init commit

---
+++ src/main/java/Lada.java
@@ -0,0 +1,6 @@
+public class Lada extends Car {
+    public Lada(String model, int year, String transmission, String color, double engineCapacity, String fuelType, String drive) {
+        super(model, year, transmission, color, "Lada", engineCapacity, fuelType, drive);
+    }
+}
+
---
+++ src/main/java/Lexus.java
@@ -0,0 +1,5 @@
+public class Lexus extends Car {
+    public Lexus(String model, int year, String transmission, String color, double engineCapacity, String fuelType, String drive) {
+        super(model, year, transmission, color, "Lexus", engineCapacity, fuelType, drive);
+    }
+}
